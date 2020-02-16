5273baab7400a4c9cb7d850090c491f2ba8ca314
jraitter <jraitter@cableone.net>
2020-02-16 16:38:09 -0700

    clock works and toggles 12/24

--- app/controllers/weather-controller.js
+++ app/controllers/weather-controller.js
@@ -9,9 +9,54 @@ function _drawWeather() {
   let weather = store.State.weather;
   document.getElementById("weather").innerHTML = weather.Template;
 }
+let clockReadOut = "12"
+let clock;
+
+function _drawClock(str) {
+  let currTime = new Date();
+  let day = convertDay(currTime.getDay());
+  let clockElem = document.getElementById("clock");
+  let hour = currTime.getHours();
+  if (str == "12") {
+    hour = ((hour > 12) ? hour - 12 : (hour == 0) ? 12 : hour);
+  }
+  let minutes = currTime.getMinutes();
+  if (minutes < 10) { minutes = "0" + minutes };
+  let seconds = currTime.getSeconds();
+  if (seconds < 10) { seconds = "0" + seconds };
+  clockElem.innerText = day + hour + ":" + minutes + ":" + seconds;
+  clock = setTimeout(_drawClock, 1000, clockReadOut);
+}
+
+function convertDay(day) {
+  switch (day) {
+    case 0:
+      return "Sunday "
+    case 1:
+      return "Monday "
+    case 2:
+      return "Tuesday "
+    case 3:
+      return "Wednesday "
+    case 4:
+      return "Thursday "
+    case 5:
+      return "Friday "
+    case 6:
+      return "Saturday"
+    default:
+      console.error("invalid day of week");
+  }
+}
 export default class WeatherController {
   constructor() {
     store.subscribe("weather", _drawWeather);
     WeatherService.getWeather();
+    _drawClock(clockReadOut);
+  }
+  toggleClock() {
+    clearTimeout(clock);
+    clockReadOut = (clockReadOut == "12") ? "24" : "12";
+    _drawClock(clockReadOut);
   }
 }
